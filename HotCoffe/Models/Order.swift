2e50ca49ffecddd8e7c2c02ce3b655c3f631e1aa
Daniel Braga <dbragabarbosa@gmail.com>
2023-01-31 10:44:51 -0300

    :construction: Criando Models

---
+++ HotCoffe/Models/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  HotCoffe
+//
+//  Created by Daniel Braga Barbosa on 31/01/23.
+//
+
+import Foundation
+
+enum CoffeType: String, Codable
+{
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeSize: String, Codable
+{
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable
+{
+    let name: String
+    let email: String
+    let type: CoffeType
+    let size: CoffeSize
+}
