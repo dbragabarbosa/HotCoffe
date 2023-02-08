fd2bbacc7acb5938049d66016528bc208dcdbfce
Daniel Braga <dbragabarbosa@gmail.com>
2023-02-08 11:54:33 -0300

    :construction: Preenchendo o View Model de tamanhos

--- HotCoffe/Controllers/AddOrderViewController.swift
+++ HotCoffe/Controllers/AddOrderViewController.swift
@@ -16,12 +16,31 @@ class AddOrderViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var tableView: UITableView!
     
+    private var coffeeSizesSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
         
 //        self.tableView.delegate = self
 //        self.tableView.dataSource = self
     }
+    
+    
+    private func setupUI() {
+        
+        self.coffeeSizesSegmentedControl = UISegmentedControl(items: self.vm.sizes)
+        self.coffeeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.coffeeSizesSegmentedControl)
+        
+        self.coffeeSizesSegmentedControl.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+        
+        self.coffeeSizesSegmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.vm.types.count
